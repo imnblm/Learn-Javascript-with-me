7ba8578e3a80d06ccced1d01308be1c957c776bd
imnblm <imeneboualami@gmail.com>
2021-08-14 14:49:20 +0100

    button

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -32,7 +32,7 @@ function addTodo(event){
     // Creation TRASH MARK BUTTON
     const trashButton = document.createElement('button')
     trashButton.innerHTML = '<i class="fas fa-trash"></i>'
-    trashButton.classList.add("complete-btn")
+    trashButton.classList.add("trash-btn")
     todoDiv.appendChild(trashButton);
 
     // APPEND TO LIST
